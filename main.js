03c976486e70facf949a366ec614701cd3547cdd
BardinPetr <bardin.petr@gmail.com>
2017-03-28 13:55:58 +0300

    Port added

--- main.js
+++ main.js
@@ -1,23 +1,26 @@
 var currentEntry = null;
 
 function init(entry) {
-    /*
-      chrome.runtime.getBackgroundPage(function(bg) {
-          if (bg.entryToLoad)
-              loadEntry(bg.entryToLoad);
-      });
-    */
-}
+    chrome.runtime.getBackgroundPage(function(bg) {
+        if (bg.entryToLoad)
+            loadEntry(bg.entryToLoad);
+    });
+};
 
 $(document).ready(init);
 
 var app = angular.module("TechLabIDE", []);
 
 app.controller("Ctrl", function($scope) {
-    $scope.init = function(){
-        $scope.ports =['test', 'test2'];
-        $scope.boards=['arduino uno', 'arduino nano', 'arduino mega', 'arduino micro'];
+    $scope.init = function() {
+        $scope.boards = ['arduino uno', 'arduino nano', 'arduino mega', 'arduino micro'];
         $scope.board = $scope.boards[0];
+
+        chrome.serial.getDevices(function(_ports) {
+            for (var i = 0; i < _ports.lenght; i++) {
+                $scope.ports[i] = _ports.path;
+            }
+        });
     };
     //Handlers for "file" dropdown menu
     $scope.new_onClick = function() {
@@ -39,7 +42,7 @@ app.controller("Ctrl", function($scope) {
     //Port
     $scope.setPort = function(name) {
         $scope.port = name;
-    };    
+    };
     $scope.setBoard = function(name) {
         $scope.board = name;
     };
