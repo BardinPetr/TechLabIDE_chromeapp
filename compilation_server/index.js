fd13de233ea5272c5c3c5ea820a6e38f11575b46
BardinPetr <bardin.petr@gmail.com>
2017-03-30 15:02:50 +0300

    Now compilation server using JSON

--- compilation_server/index.js
+++ compilation_server/index.js
@@ -6,73 +6,92 @@ var http = require('http'),
     path = require('path'),
     shell = require('shelljs');
 
-function randomInt (low, high) {
+function randomInt(low, high) {
     return Math.floor(Math.random() * (high - low) + low);
 }
 
-http.createServer(function(request, response) {  
-    response.setHeader('Access-Control-Allow-Origin', '*');
-    response.setHeader('Access-Control-Allow-Methods', 'GET, POST');
-    response.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-    response.setHeader('Access-Control-Allow-Credentials', true);
+http.createServer(function(request, response) {
+        response.setHeader('Access-Control-Allow-Origin', '*');
+        response.setHeader('Access-Control-Allow-Methods', 'GET, POST');
+        response.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+        response.setHeader('Access-Control-Allow-Credentials', true);
 
-    var data = request.url
-    data = data.replace(/%20/g, ' ');
-    data = data.replace(/%0A/g, '\n');
+        var data = request.url
 
-    if(data == '/favicon.ico'){
-        response.writeHead(200);
-    }
-    else{
-        arduinopath = arduinopathC;
-        var rnd = "s" + randomInt(0, 99999).toString();
-        var rnd_o = "o" + rnd;
-        var s_Path = rnd + "/" + rnd + ".ino";
-        var o_Path = rnd_o + "\\";
-        var h_Path = rnd_o + "/" + rnd + ".ino.with_bootloader.hex";
+        if (data == '/favicon.ico') {
+            response.writeHead(200);
+        } else {
+            data = data.replace(/%20/g, ' ');
+            data = data.replace(/%22/g, '\"');
+            data = data.replace(/%27/g, '\'');
+            data = data.substring(7);
 
-        var board = data.substring(4, data.indexOf("&s="));
-        var sketch = data.substring(data.indexOf("&s=")+3);
+            arduinopath = arduinopathC;
+            var rnd = "s" + randomInt(0, 99999).toString();
+            var rnd_o = "o" + rnd;
+            var s_Path = rnd + "/" + rnd + ".ino";
+            var o_Path = rnd_o + "\\";
+            var h_Path = rnd_o + "/" + rnd + ".ino.with_bootloader.hex";
 
-        shell.exec("mkdir " + rnd, {silent:true});
-        shell.exec("mkdir " + rnd_o, {silent:true});
+            var json = JSON.parse(data);
+            var board = json.board;
+            var sketch = json.sketch;
+            sketch = sketch.replace(/%0A/g, '\n');
 
-        
-        fileSystem.writeFile(s_Path, sketch, function(err) {
-            if(err) {
-                return console.log(err);
-            }
-            
-            arduinopath += (" --board " + board) + (" --pref build.path=" + rnd_o) + (" --verify " + s_Path); 
-            shell.exec(arduinopath, function(code, stdout, stderr) {                
-                if (code == 0){
-                    var filePath = path.join(__dirname, h_Path);
-                    var stat = fileSystem.statSync(filePath);
+            shell.exec("mkdir " + rnd, { silent: true });
+            shell.exec("mkdir " + rnd_o, { silent: true });
 
-                    response.writeHead(200, {
-                        'Content-Type': 'text/hex', 
-                        'Content-Length': stat.size
-                    });
-                    
-                    var readStream = fileSystem.createReadStream(filePath);
-                    readStream.on('data', function(data) {
-                        response.write(data);
-                    });
-                    
-                    readStream.on('end', function() {
-                        response.end();      
+            fileSystem.writeFile(s_Path, sketch, function(err) {
+                if (err) {
+                    return console.log(err);
+                }
+
+                arduinopath += (" --board " + board) + (" --pref build.path=" + rnd_o) + (" --verify " + s_Path);
+                shell.exec(arduinopath, function(code, stdout, stderr) {
+                    if (code == 0) {
+                        var filePath = path.join(__dirname, h_Path);
+
+                        fileSystem.readFile(h_Path, 'utf8', function(err, data) {
+                            if (err) {
+                                return log(err);
+                            }
+
+                            var output = { "code": code, "hex": data, "stderr": null, "stdout": stdout }
+                            var _output = JSON.stringify(output);
+
+                            response.writeHead(200, {
+                                'Content-Type': 'application/json',
+                                'Content-Length': _output.length
+                            });
+                            response.write(_output);
+
+                            shell.rm('-rf', rnd);
+                            shell.rm('-rf', rnd_o);
+                        });
+                    } else {
+                        stderr = stderr.replace(/Loading configuration.../g, '');
+                        stderr = stderr.replace(/\r\nInitializing packages.../g, '');
+                        stderr = stderr.replace(/\r\nPreparing boards.../g, '');
+                        stderr = stderr.replace(/\r\nVerifying...\r\n/g, '');
+
+                        var output = { "code": code, "hex": null, "stderr": stderr, "stdout": stdout }
+                        var _output = JSON.stringify(output);
+
+                        response.writeHead(200, {
+                            'Content-Type': 'application/json',
+                            'Content-Length': _output.length
+                        });
+                        response.write(_output);
+
                         shell.rm('-rf', rnd);
-                        shell.rm('-rf', rnd_o);  
-                    });
-                }
-                else{
-                    response.writeHead(403);
-                    
-                    shell.rm('-rf', rnd);
-                    shell.rm('-rf', rnd_o);  
-                }
+                        shell.rm('-rf', rnd_o);
+                    }
+                });
             });
-        }); 
-    }
-})
-.listen(2000);
+        }
+    })
+    .listen(2000);
+
+function log(data) {
+    console.log(data);
+}
